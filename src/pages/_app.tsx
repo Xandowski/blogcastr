8d03bff36b3ed024e02ed19e76b97c58d9db65ca
Xandowski <xandy-moraes@hotmail.com>
2021-04-21 00:58:43 -0300

    added some global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import AppProps from 'next/app'
+import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,23 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "white": "var(--white)",
+        "gray-50": "var(--gray-50)",
+        "gray-100": "var(--gray-100)",
+        "gray-200": "var(--gray-200)",
+        "gray-500": "var(--gray-500)",
+        "gray-800": "var(--gray-800)",
+
+        "green-500": "var(--green-500)",
+
+        "purple-300": "var(--purple-300)",
+        "purple-400": "var(--purple-400)",
+        "purple-500": "var(--purple-500)",
+        "purple-800": "var(--purple-800)"
+      }
+    },
   },
   variants: {
     extend: {},
